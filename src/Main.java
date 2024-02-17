30e64a57aed75f41d7716f769f3b92c29f3383b1
Reckse7 <suvor.pro@gmail.com>
2024-02-17 06:51:23 +0300

    update calculator

--- src/Main.java
+++ src/Main.java
@@ -1,120 +1,112 @@
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        String input, output;
         Scanner scan = new Scanner(System.in);
         System.out.println("Input:");
-        input = scan.nextLine();
-        output = calc(input);
-        System.out.println("Output:\n" + output);
-
+        String input = scan.nextLine();
+        System.out.println("Output:\n" + calc(input));
     }
 
     public static String calc(String input) {
+        String[] exp = Calculator.getExpression(input);
+        boolean isRoman = Calculator.checkRoman(exp[0], exp[2]);
+        int result = Calculator.calculate(isRoman, exp);
+        return Calculator.toString(isRoman, result);
+    }
+}
 
-        String [] romanTens = {"X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C"};
-        String [] romanUnits = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
-        int i = -1, arg1 = 0, arg2 = 0, k;
-        String resLine = new String();
-        String[] strings = new String[2];
-        boolean checkRoman = false;
+class Calculator {
 
-        char[] operator = {'+', '-', '*', '/'};
-        for (char element: operator) {
-            i = input.indexOf(element);
-            if(i>=0) {
-                String str = new StringBuilder().append("\\").append(element).toString();
-                strings = input.split(str, 2);
+    private final static String[] romanTens = {"X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C"};
+    private final static String[] romanUnits = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+    private final static String[] operators = {"-", "*", "/", "+"};
+
+    public static String[] getExpression(String input) {
+        String operator = "";
+        String[] strings = input.split("[+\\-*/]", 2);
+        for (String op : operators) {
+            if (input.contains(op)) {
+                operator = op;
                 break;
             }
         }
-        if( i < 0)
-            try {
-                throw new IOException();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
-        String num1 = strings[0];
-        String num2 = strings[1];
-        num1 = num1.trim();
-        num2 = num2.trim();
-        for ( k=0; k<romanUnits.length; k++) {
-            checkRoman = num1.equalsIgnoreCase(romanUnits[k]);
-            if(checkRoman) {
-                arg1 = k+1;
-                break;
-            }
+        if (operator.isEmpty()) {
+            throw new RuntimeException("Недопустимое выражение");
         }
-        if(checkRoman)
-            for (k=0; k<romanUnits.length; k++) {
-                checkRoman = num2.equalsIgnoreCase(romanUnits[k]);
-                if(checkRoman) {
-                    arg2 = k+1;
+        return new String[] {strings[0].trim(), operator, strings[1].trim()};
+    }
+
+    public static boolean checkRoman(String... args) {
+        for (String arg : args) {
+            boolean isRoman = false;
+            for (String romanUnit : romanUnits) {
+                if (arg.equalsIgnoreCase(romanUnit)) {
+                    isRoman = true;
                     break;
                 }
             }
-        if(!checkRoman) {
-            try {
-                Integer int1 = Integer.valueOf(num1);
-                arg1 = int1;
-            } catch (NumberFormatException e) {
-                throw new RuntimeException(e);
+            if (!isRoman) {
+                return false;
             }
-            try {
-                Integer int2 = Integer.valueOf(num2);
-                arg2 = int2;
-            } catch (NumberFormatException e) {
-                throw new RuntimeException(e);
-            }
-
-            if(arg1 < 1 || arg1 > 10 || arg2 < 1 || arg2 > 10)
-                try {
-                    throw new IOException();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
         }
-
-        int result = 0;
-        switch(input.charAt(i)) {
-            case '+':
-                result = arg1 + arg2;
-                break;
-            case '-':
-                result = arg1 - arg2;
-                break;
-            case '*':
-                result = arg1 * arg2;
-                break;
-            case'/':
-                result = arg1 / arg2;
-                break;
-        }
-
-        if(checkRoman)
-        {
-            if(result < 1)
-                try {
-                    throw new IOException();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            int multiplier;
-            multiplier = result/10;
-            if(multiplier > 0)
-                resLine += romanTens[multiplier -1];
-            multiplier = result % 10;
-            if(multiplier > 0)
-                resLine += romanUnits[multiplier -1];
-            return resLine;
-        }
-
-        resLine = Integer.toString(result);
-        return resLine;
+        return true;
     }
 
+    public static int calculate(boolean isRoman, String[] expression) {
+        if (expression.length != 3) {
+            throw new RuntimeException();
+        }
+        int num1 = convert(expression[0], isRoman);
+        int num2 = convert(expression[2], isRoman);
+        return switch (expression[1]) {
+            case "+" -> num1 + num2;
+            case "-" -> num1 - num2;
+            case "*" -> num1 * num2;
+            case "/" -> num1 / num2;
+            default -> throw new RuntimeException("Недопустимый оператор");
+        };
+    }
+
+    private static int convert(String arg, boolean isRoman) {
+        return isRoman ? convertToRoman(arg) : convertToArabic(arg);
+    }
+
+    private static int convertToRoman(String arg) {
+        for (int k = 0; k < romanUnits.length; k++) {
+            if (arg.equalsIgnoreCase(romanUnits[k])) {
+                return k + 1;
+            }
+        }
+        throw new RuntimeException("Ошибка конвертации");
+    }
+
+    private static int convertToArabic(String arg) {
+        int i = Integer.parseInt(arg);
+        if (i < 1 || i > 10) {
+            throw new RuntimeException("Допустимы числа от 1 до 10");
+        }
+        return i;
+    }
+
+    public static String toString(boolean isRoman, int arg) {
+        return isRoman ? toRomanString(arg) : Integer.toString(arg);
+    }
+
+    private static String toRomanString(int arg) {
+        int multiplier;
+        StringBuilder result = new StringBuilder();
+        if (arg < 1) {
+            throw new RuntimeException("Результат должен быть больше нуля");
+        }
+        multiplier = arg / 10;
+        if (multiplier > 0) {
+            result.append(romanTens[multiplier - 1]);
+        }
+        multiplier = arg % 10;
+        if (multiplier > 0) {
+            result.append(romanUnits[multiplier - 1]);
+        }
+        return result.toString();
+    }
 }
